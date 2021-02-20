80609856a26e74a88e1d6f57941c7b551c1a11cc
Justin Starry <justin@solana.com>
2021-02-20 16:56:17 +0800

    Finish step 1

--- step-1/src/main.rs
+++ step-1/src/main.rs
@@ -6,38 +6,38 @@ use std::str::FromStr;
 
 mod utils;
 
+// Used in Step 1
 const MEMO_PROGRAM_ID: &str = "Memo1UhkJRfHyvLMcVucJwxXeuD728EqVDDwQDxFMNo";
-const ORDER_ID: u64 = 1606954633;
+const EVENTBRITE_ORDER_ID: u64 = 0;
 
 fn main() {
     let fee_payer = utils::load_config_keypair();
     let rpc_client = utils::new_rpc_client();
-    let memo = format!("GIMME STICKER: {}", ORDER_ID);
+    let memo = format!("GIMME STICKER: {}", EVENTBRITE_ORDER_ID);
 
     // Step 1: Create an SPL Memo instruction
-    let memo_instruction = Instruction::new(
-        Pubkey::from_str(MEMO_PROGRAM_ID).unwrap(),
-        &memo,
-        Vec::new(),
-    );
+    // Doc hints:
+    //  - https://docs.rs/solana-sdk/1.5.8/solana_sdk/instruction/struct.Instruction.html#method.new
+    //  - https://docs.rs/solana-sdk/1.5.8/solana_sdk/pubkey/struct.Pubkey.html#impl-FromStr
 
-    // Step 2: Fetch a recent blockhash
-    let (recent_blockhash, _fee_calculator) = rpc_client
-        .get_recent_blockhash()
-        .expect("failed to get recent blockhash");
+    // Step 2: Create a transaction
+    // Doc hints:
+    //  - https://docs.rs/solana-sdk/1.5.8/solana_sdk/transaction/struct.Transaction.html#method.new_with_payer
 
-    // Step 3: Create a transaction
-    let tx = Transaction::new(
-        &[&fee_payer],
-        Message::new(&[memo_instruction], Some(&fee_payer.pubkey())),
-        recent_blockhash,
-    );
+    // Step 3: Fetch a recent blockhash
+    // Doc hints:
+    //  - https://docs.rs/solana-client/1.5.8/solana_client/rpc_client/struct.RpcClient.html#method.get_recent_blockhash
 
-    // Step 4: Send transaction
-    rpc_client
-        .send_and_confirm_transaction_with_spinner(&tx)
-        .expect("tx failed");
+    // Step 4: Sign transaction
+    // Doc hints:
+    //  - https://docs.rs/solana-sdk/1.5.8/solana_sdk/transaction/struct.Transaction.html#method.sign
 
-    println!("Created Memo: {}", memo);
-    println!("Transaction Signature: {}", utils::tx_signature(&tx));
+    // Step 5: Send transaction
+    // Doc hints:
+    //  - https://docs.rs/solana-client/1.5.8/solana_client/rpc_client/struct.RpcClient.html#method.send_and_confirm_transaction_with_spinner
+
+    // Step 6: Uncomment and run `cargo run`
+    //  - Note: be sure your transaction variable is named `tx`
+    // println!("Created Memo: {}", memo);
+    // println!("Transaction Signature: {}", utils::tx_signature(&tx));
 }
