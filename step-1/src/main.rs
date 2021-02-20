b6b0400046e6a7c4fe4c5f8c97203394f6dc8639
Justin Starry <justin@solana.com>
2021-02-20 16:58:54 +0800

    streamline

--- step-1/src/main.rs
+++ step-1/src/main.rs
@@ -1,5 +1,5 @@
 use solana_sdk::{
-    instruction::Instruction, message::Message, pubkey::Pubkey, signature::Signer,
+    instruction::Instruction, pubkey::Pubkey, signature::Signer,
     transaction::Transaction,
 };
 use std::str::FromStr;
@@ -8,35 +8,60 @@ mod utils;
 
 // Used in Step 1
 const MEMO_PROGRAM_ID: &str = "Memo1UhkJRfHyvLMcVucJwxXeuD728EqVDDwQDxFMNo";
+// Enter your own order id to receive a prize!
 const EVENTBRITE_ORDER_ID: u64 = 0;
 
 fn main() {
-    let fee_payer = utils::load_config_keypair();
-    let rpc_client = utils::new_rpc_client();
-    let memo = format!("GIMME STICKER: {}", EVENTBRITE_ORDER_ID);
+    let keypair = utils::load_config_keypair();
+    let pubkey = keypair.pubkey();
 
     // Step 1: Create an SPL Memo instruction
+    //
+    //   Each Solana instruction includes a program id, list of account inputs,
+    //   and a data vector. Memo instructions don't require any account inputs!
+    //
     // Doc hints:
     //  - https://docs.rs/solana-sdk/1.5.8/solana_sdk/instruction/struct.Instruction.html#method.new
     //  - https://docs.rs/solana-sdk/1.5.8/solana_sdk/pubkey/struct.Pubkey.html#impl-FromStr
+    let memo = format!("GIMME STICKER: {}", EVENTBRITE_ORDER_ID);
 
-    // Step 2: Create a transaction
+    // Step 2: Create a transaction with payer
+    // 
+    //   All transactions must pay fees!
+    //
     // Doc hints:
     //  - https://docs.rs/solana-sdk/1.5.8/solana_sdk/transaction/struct.Transaction.html#method.new_with_payer
+    let fee_payer = Some(&pubkey);
 
     // Step 3: Fetch a recent blockhash
+    //  
+    //   In order to prevent duplicate transactions (imagine you pay 50NT for coffee every day)
+    //   each transaction includes a recent blockhash. This blockhash also gives transactions a lifetime
+    //   because a blockhash is only "recent" for about 2 minutes.
+    //
     // Doc hints:
     //  - https://docs.rs/solana-client/1.5.8/solana_client/rpc_client/struct.RpcClient.html#method.get_recent_blockhash
+    let rpc_client = utils::new_rpc_client();
 
     // Step 4: Sign transaction
+    //
+    //   Prove that you approve the transaction by signing with your keypair!
+    //
     // Doc hints:
     //  - https://docs.rs/solana-sdk/1.5.8/solana_sdk/transaction/struct.Transaction.html#method.sign
 
     // Step 5: Send transaction
+    //
+    //   Once signed, send your transaction to the Solana cluster for processing
+    //
     // Doc hints:
     //  - https://docs.rs/solana-client/1.5.8/solana_client/rpc_client/struct.RpcClient.html#method.send_and_confirm_transaction_with_spinner
 
     // Step 6: Uncomment and run `cargo run`
+    //
+    //   This will print the signature of your transaction. Copy and paste the signature into
+    //   the workshop block explorer to see if it was successful: https://defi-workshop.netlify.app/
+    //
     //  - Note: be sure your transaction variable is named `tx`
     // println!("Created Memo: {}", memo);
     // println!("Transaction Signature: {}", utils::tx_signature(&tx));
