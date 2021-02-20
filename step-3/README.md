eb3f8e18dc790deb538199083076caa1d63f9c2d
Justin Starry <justin@solana.com>
2021-02-20 23:17:05 +0800

    step 3

--- step-3/README.md
+++ step-3/README.md
@@ -1,12 +1,71 @@
 # Step 3 - Deploy an on-chain program
 
 The goal of this step is to use Rust to write and deploy
-a program on Solana.
+a program on Solana that allows users to redeem stickers.
 
-## Task 3B - Code!
+### Context
 
-Time to start coding! Open the `/step-3/src/main.rs` file which contains
-instructions for deploying your first program on the Solana blockchain.
+On Solana, an on-chain program is an executable that can be called
+to change account state on the blockchain. Programs are always called
+with a list of accounts and raw instruction data. It's up to each
+program to decide which accounts are required, whether instruction data
+is valid or not, etc.
+
+Typically developers write programs using Rust and use the Solana tools
+to compile their program and deploy it to the blockchain at an address
+called the "program id". Once deployed, anyone can call your program by
+creating an instruction that includes your program id. If you made a
+mistake or wish to change behavior, you may redeploy your program to the
+program id.
+
+### Program entrypoint
+
+Each program has a single entrypoint which can return success or failure.
+The entrypoint includes `program_id` which is useful for checking if an
+account is assigned to your program (meaning you can write to it!). It also
+includes a list of `AccountInfo` which will provide each account input's
+public key, account metadata, and whether or not the account signed the
+instruction. Lastly, it includes raw instruction data which can be handled
+however the program would like.
+
+```rust
+fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult { .. }
+```
+
+### Sticker Redeemer
+
+For this task, you will learn how to create a program which redeems
+sticker tokens. To redeem a sticker token, it must be destroyed by sending
+a "Burn" instruction to the SPL Token program.
+
+## Task 3B - Code your program!
+
+Time to start coding! Open the `/step-3/program/src/lib.rs` file which contains
+instructions for writing your first Solana program.
+
+When you're finished, run the following command:
+
+```sh
+$ cargo build-bpf
+```
+
+## Task 3C - Deploy your program!
+
+After building your program, you can deploy it to the blockchain with
+the following command:
+
+```sh
+$ solana program deploy ./target/deploy/solana_workshop_tw_step_3.so
+```
+
+## Task 4C - Call your program!
+
+Keep on coding! Open the `/step-3/client/src/main.rs` file which contains
+instructions for calling your Solana program.
 
 When you're finished, run the following command:
 
---
+++ step-3/program/src/lib.rs
@@ -0,0 +1,106 @@
+use solana_program::{
+    account_info::{next_account_info, AccountInfo},
+    entrypoint,
+    entrypoint::ProgramResult,
+    msg,
+    program::invoke,
+    pubkey::Pubkey,
+};
+
+// Enter your own order id to receive a prize!
+const EVENTBRITE_ORDER_ID: u64 = 0;
+
+entrypoint!(process_instruction);
+fn process_instruction(
+    _program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    _instruction_data: &[u8],
+) -> ProgramResult {
+    msg!("Sticker Redeemer!");
+    msg!("Eventbrite order id: {}", EVENTBRITE_ORDER_ID);
+
+    // Step 1: Extract account inputs
+    //
+    //   Expected account inputs:
+    //    1. My sticker token address
+    //    2. Sticker mint address
+    //    3. My pubkey
+    //
+    // Doc hints:
+    //  - https://docs.rs/solana-program/1.5.8/solana_program/account_info/fn.next_account_info.html
+    let account_info_iter = &mut accounts.iter();
+    let token_account_info = next_account_info(account_info_iter)?;
+    let mint_account_info = next_account_info(account_info_iter)?;
+    let authority_account_info = next_account_info(account_info_iter)?;
+
+    // Step 2: Create "Burn" instruction for SPL Token program
+    //
+    //  Tips:
+    //   - This instruction should burn exactly one token.
+    //   - Pass `&[]` as `signer_keys` it's not important here
+    //
+    // Doc hints:
+    //  - https://docs.rs/spl-token/3.1.0/spl_token/instruction/fn.burn.html
+    let redeem_sticker_instruction = spl_token::instruction::burn(
+        &spl_token::ID,
+        token_account_info.key,
+        mint_account_info.key,
+        authority_account_info.key,
+        &[],
+        1,
+    )
+    .unwrap();
+
+    msg!("Redeeming sticker...");
+
+    // Step 3: Invoke the "Burn" instruction for SPL Token program
+    //
+    //  Tips:
+    //   - Each account info must be cloned
+    //
+    // Doc hints:
+    //  - https://docs.rs/solana-program/1.5.8/solana_program/program/fn.invoke.html
+    invoke(
+        &redeem_sticker_instruction,
+        &[
+            token_account_info.clone(),
+            mint_account_info.clone(),
+            authority_account_info.clone(),
+        ],
+    )?;
+
+    // Step 4: Create "Close Account" instruction for SPL Token program
+    //
+    //  Now that the token has been burned, we don't the token account anymore.
+    //  The remaining lamports can be reclaimed by closing the account.
+    //
+    // Doc hints:
+    //  - https://docs.rs/spl-token/3.1.0/spl_token/instruction/fn.close_account.html
+    let close_sticker_account_ix = spl_token::instruction::close_account(
+        &spl_token::ID,
+        token_account_info.key,
+        authority_account_info.key,
+        authority_account_info.key,
+        &[],
+    ).unwrap();
+
+    msg!("Closing sticker account...");
+
+    // Step 5: Invoke the "Close Account" instruction for SPL Token program
+    //
+    //  Tips:
+    //   - Each account info must be cloned
+    //
+    // Doc hints:
+    //  - https://docs.rs/solana-program/1.5.8/solana_program/program/fn.invoke.html
+    invoke(
+        &close_sticker_account_ix,
+        &[
+            token_account_info.clone(),
+            authority_account_info.clone(),
+        ],
+    )?;
+
+    msg!("Redeem succeeded!");
+    Ok(())
+}
